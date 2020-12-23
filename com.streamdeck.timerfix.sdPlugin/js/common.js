0fc7f463ab12f17f7bcbfcb1cbc407b9f1393336
Andy Fuchs <andy@foxwerk.de>
2020-12-23 11:07:34 +0100

    merge latest changes of common utils

--- com.streamdeck.timerfix.sdPlugin/js/common.js
+++ com.streamdeck.timerfix.sdPlugin/js/common.js
@@ -6,8 +6,10 @@
   wrap-iife: 0,
   one-var: 0,
   no-var: 0,
-  vars-on-top: 0
+  vars-on-top: 0,
+  quotes: ["error", "single"]
 */
+/*eslint-env es6*/
 
 // don't change this to let or const, because we rely on var's hoisting
 // eslint-disable-next-line no-use-before-define, no-var
@@ -21,8 +23,7 @@ var $localizedStrings = $localizedStrings || {},
     // eslint-disable-next-line no-unused-vars
     isQT = navigator.appVersion.includes('QtWebEngine'),
     debug = debug || false,
-    debugLog = function () {},
-    MIMAGECACHE = MIMAGECACHE || {};
+    debugLog = function() {};
 
 const setDebugOutput = debug => (debug === true ? console.log.bind(window.console) : function() {});
 debugLog = setDebugOutput(debug);
@@ -79,20 +80,10 @@ var Utils = {
         return typeof value === 'undefined';
     },
     isObject: function (o) {
-        return (
-            typeof o === 'object' &&
-            o !== null &&
-            o.constructor &&
-            o.constructor === Object
-        );
+        return typeof o === 'object' && o !== null && o.constructor && o.constructor === Object;
     },
     isPlainObject: function (o) {
-        return (
-            typeof o === 'object' &&
-            o !== null &&
-            o.constructor &&
-            o.constructor === Object
-        );
+        return typeof o === 'object' && o !== null && o.constructor && o.constructor === Object;
     },
     isArray: function (value) {
         return Array.isArray(value);
@@ -186,12 +177,8 @@ Utils.randomString = function (len = 8) {
     return Array.apply(0, Array(len))
         .map(function () {
             return (function (charset) {
-                return charset.charAt(
-                    Math.floor(Math.random() * charset.length)
-                );
-            })(
-                'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789'
-            );
+                return charset.charAt(Math.floor(Math.random() * charset.length));
+            })('ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789');
         })
         .join('');
 };
@@ -204,9 +191,7 @@ Utils.randomLowerString = function (len = 8) {
     return Array.apply(0, Array(len))
         .map(function () {
             return (function (charset) {
-                return charset.charAt(
-                    Math.floor(Math.random() * charset.length)
-                );
+                return charset.charAt(Math.floor(Math.random() * charset.length));
             })('abcdefghijklmnopqrstuvwxyz');
         })
         .join('');
@@ -309,10 +294,17 @@ Utils.fadeColor = function (col, amt) {
     const min = Math.min, max = Math.max;
     const num = parseInt(col.replace(/#/g, ''), 16);
     const r = min(255, max((num >> 16) + amt, 0));
-    const g = min(255, max((num & 0x0000FF) + amt, 0));
-    const b = min(255, max(((num >> 8) & 0x00FF) + amt, 0));
+    const g = min(255, max((num & 0x0000ff) + amt, 0));
+    const b = min(255, max(((num >> 8) & 0x00ff) + amt, 0));
     return '#' + (g | (b << 8) | (r << 16)).toString(16).padStart(6, 0);
+};
+
+Utils.lerpColorWithScale = function(startColor, targetColor, amount, scale = 0.5) {
+    if(amount < scale) {
+        return Utils.lerpColor(startColor, '#FFF2EC', amount * 2);
 }
+    return Utils.lerpColor('#FFF2EC', targetColor, amount);
+};
 
 Utils.lerpColor = function (startColor, targetColor, amount) {
     const ah = parseInt(startColor.replace(/#/g, ''), 16);
@@ -346,8 +338,8 @@ Utils.hexToRgb = function (hex) {
 };
 
 Utils.rgbToHex = (r, g, b) => '#' + [r, g, b].map(x => {
-    return x.toString(16).padStart(2,0)
-}).join('')
+    return x.toString(16).padStart(2, 0);
+}).join('');
 
 
 Utils.nscolorToRgb = function (rP, gP, bP) {
@@ -373,7 +365,19 @@ Utils.kelvinToMired = function (kelvin, roundTo) {
 
 Utils.roundBy = function(num, x) {
     return Math.round((num - 10) / x) * x;
+};
+
+Utils.quantizeNumber = function(val, quantum, {cover = false} = {}) {
+    if(!quantum) {
+        return 0;
 }
+    var remainder = val % quantum;
+    // I'm intentionally not using Math.sign so that no polyfill is
+    // required to use this library in legacy environments.
+    var sign = val >= 0 ? 1 : -1;
+    var mod = cover && remainder ? quantum : 0;
+    return val - remainder + sign * mod;
+};
 
 Utils.getBrightness = function (hexColor) {
     // http://www.w3.org/TR/AERT#color-contrast
@@ -426,6 +430,13 @@ Utils.loadScript = function (url, callback) {
         callback(url, false);
     };
     document.body.appendChild(el);
+};
+
+Utils.createInlineWorker = (fn) => {
+    const fnAsString = fn.toString().replace(/^[^{]*{\s*/, '').replace(/\s*}[^}]*$/, '');
+    return new Worker(URL.createObjectURL(
+        new Blob([fnAsString], {type: 'text/javascript'})
+    ));
 };
 
 Utils.parseJson = function (jsonString) {
@@ -520,24 +531,38 @@ Utils.setProp = function (jsonObj, path, value) {
     return jsn;
 };
 
-Utils.getDataUri = function (url, callback, inCanvas, inFillcolor) {
+Utils.stringToHTML = function(html, all = false) {
+    var template = document.createElement('template');
+    template.innerHTML = html.trim();
+    return all ? template.content : template.content.firstChild;
+};
+
+Utils.stringToHTMLDiv = function(html, className) {
+    var template = document.createElement('div');
+    if(className) template.classList.add(className);
+    template.innerHTML = html.trim();
+    return template;
+};
+
+
+Utils.getDataUri = function(url, callback, inCanvas, inFillcolor, w, h, clearCtx) {
     var image = new Image();
 
     image.onload = function () {
-        const canvas =
-            inCanvas && Utils.isCanvas(inCanvas)
-                ? inCanvas
-                : document.createElement('canvas');
+        const canvas = inCanvas && Utils.isCanvas(inCanvas) ? inCanvas : document.createElement('canvas');
 
-        canvas.width = this.naturalWidth; // or 'width' if you want a special/scaled size
-        canvas.height = this.naturalHeight; // or 'height' if you want a special/scaled size
+        canvas.width = w || this.naturalWidth; // or 'width' if you want a special/scaled size
+        canvas.height = h || this.naturalHeight; // or 'height' if you want a special/scaled size
 
         const ctx = canvas.getContext('2d');
+        if(clearCtx) {
+            ctx.clearRect(0, 0, canvas.width, canvas.height);
+        }
         if (inFillcolor) {
             ctx.fillStyle = inFillcolor;
             ctx.fillRect(0, 0, canvas.width, canvas.height);
         }
-        ctx.drawImage(this, 0, 0);
+        ctx.drawImage(image, 0, 0);
         // Get raw image data
         // callback && callback(canvas.toDataURL('image/png').replace(/^data:image\/(png|jpg);base64,/, ''));
 
@@ -546,6 +571,129 @@ Utils.getDataUri = function (url, callback, inCanvas, inFillcolor) {
     };
 
     image.src = url;
+};
+
+Utils.s2c = function(svg, inCanvas, inContext, w, h, cb) {
+    var img = new Image();
+    img.src = svg;
+    img.onload = function() {
+        let cnv = inCanvas || document.createElement('canvas');
+        let ctx = inContext || cnv.getContext('2d');
+        cnv.width = w || image.naturalWidth;
+        cnv.height = h || image.naturalHeight;
+        ctx.clearRect(0, 0, cnv.width, cnv.height);
+        ctx.drawImage(img, 0, 0);
+        if(cb) cb(this, cnv.toDataURL('image/png'));
+    };
+};
+
+Utils.drawStringToCanvas = (text, size = 400, inCanvas) => {
+    const canvas = inCanvas ? inCanvas : document.createElement('canvas');
+    const tempCtx = canvas.getContext('2d');
+    tempCtx.clearRect(0, 0, canvas.width, canvas.height);
+    tempCtx.font = `${size}px Helvetica`;
+    tempCtx.fontWeight = 'bold';
+    tempCtx.lineHeight = size;
+    tempCtx.fillStyle = '#d8d8d8';
+    tempCtx.textAlign = 'center';
+    tempCtx.textBaseline = 'middle';
+    // tempCtx.fillText(`${text}`, canvas.width / 2, canvas.height / 2);
+
+    // var text = 'All the world \'s a stage, and all the men and women merely players. They have their exits and their entrances; And one man in his time plays many parts.';
+    // wrapText(tempCtx, text, 0, 0, 144);
+    Utils.wrapText(tempCtx, text, canvas.width / 2, canvas.height / 2, canvas.width);
+
+
+};
+
+Utils.wrapText = (ctx, text, x = 0, y = 0, maxWidth = 144) => {
+    const words = text.split(' ');
+    let line = '';
+    let metrics;
+
+    for(let i = 0;i < words.length;i++) {
+        let tmpStr = words[i];
+        metrics = ctx.measureText(tmpStr);
+        while(metrics.width > maxWidth) {
+            tmpStr = tmpStr.substring(0, tmpStr.length - 1);
+            metrics = ctx.measureText(tmpStr);
+        }
+
+        if(words[i] != tmpStr) {
+            words.splice(i + 1, 0, words[i].substr(tmpStr.length));
+            words[i] = tmpStr;
+        }
+
+        tmpStr = words.length > 1 ? `${line}${words[i]} ` : `${line}${words[i]}`;
+        metrics = ctx.measureText(tmpStr);
+        // console.log("len:", words.length, metrics);
+
+        if(metrics.width > maxWidth && i > 0) {
+            y -= ctx.lineHeight / 2;
+            ctx.fillText(line, x, y);
+            line = `${words[i]} `;
+            y += ctx.lineHeight;
+        }
+        else {
+            // console.log(tmpStr, metrics.width, x, maxWidth);
+            // x = (metrics.width + x) / 2;
+            line = tmpStr;
+        }
+    }
+    ctx.fillText(line, x, y);
+};
+
+Utils.getFontSize = (options = {}) => { //thx gifshot! :)
+    // var options = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : {};
+    /* needs {
+        text: options.title, // the text to measure
+        width: options.width || window.innerWidth, // maximum available width
+        minFontSize: options.minFontSize, //minimum acceptable font size
+    }
+    */
+    if(!document.body || options.resizeFont === false) {
+        return options.fontSize;
+    }
+
+    var fontSize = parseInt(options.fontSize, 10) || 13;
+    var minFontSize = parseInt(options.minFontSize, 10) || 6;
+    var div = document.createElement('div');
+    var span = document.createElement('span');
+
+    div.setAttribute('width', options.width);
+    div.appendChild(span);
+
+    span.innerHTML = options.title || options.text || '';
+    span.style.fontSize = fontSize + 'px';
+    span.style.textIndent = '-9999px';
+    span.style.visibility = 'hidden';
+
+    document.body.appendChild(span);
+
+    while((span.offsetWidth > options.width) && (fontSize >= minFontSize)) {
+        span.style.fontSize = --fontSize + 'px';
+    }
+
+    document.body.removeChild(span);
+
+    return fontSize; //+ 'px';
+};
+
+
+// const greenKey = Utils.createColoredKeyAsDataUrl(null, "#00AA33");
+
+Utils.createColoredKeyAsDataUrl = (inCanvas, inFillcolor, inWidth = 144, inHeight = 144) => {
+    let canvas = inCanvas;
+    if(!inCanvas) {
+        canvas = document.createElement('canvas');
+        canvas.width = inWidth;
+        canvas.height = inHeight;
+    }
+    const tempCtx = canvas.getContext('2d');
+    tempCtx.clearRect(0, 0, canvas.width, canvas.height);
+    tempCtx.fillStyle = inFillcolor;
+    tempCtx.fillRect(0, 0, canvas.width, canvas.height);
+    return canvas.toDataURL('image/png');
 };
 
 /** Quick utility to inject a style to the DOM
@@ -607,9 +755,7 @@ Utils.loadImageWithOptions = (url, w, h, inCanvas, clearCtx, inFillcolor) =>
 Utils.loadImage = function (inUrl, callback, inCanvas, inFillcolor) {
     /** Convert to array, so we may load multiple images at once */
     const aUrl = !Array.isArray(inUrl) ? [inUrl] : inUrl;
-    const canvas = inCanvas && inCanvas instanceof HTMLCanvasElement
-        ? inCanvas
-        : document.createElement('canvas');
+    const canvas = inCanvas && inCanvas instanceof HTMLCanvasElement ? inCanvas : document.createElement('canvas');
     var imgCount = aUrl.length - 1;
     const imgCache = {};
 
@@ -646,7 +792,7 @@ Utils.loadImage = function (inUrl, callback, inCanvas, inFillcolor) {
                     }
                 });
 
-                callback(canvas.toDataURL('image/png'));
+                callback(canvas.toDataURL('image/png'), canvas.width, canvas.height, this);
                 // or to get raw image data
                 // callback && callback(canvas.toDataURL('image/png').replace(/^data:image\/(png|jpg);base64,/, ''));
             }
@@ -655,6 +801,18 @@ Utils.loadImage = function (inUrl, callback, inCanvas, inFillcolor) {
         imgCount--;
         image.src = url;
     }
+};
+
+Utils.crop = function(canvas, offsetX, offsetY, width, height, callback, inCanvas) {
+    var buffer = inCanvas && inCanvas instanceof HTMLCanvasElement ? inCanvas : document.createElement('canvas');
+    var ctx = buffer.getContext('2d');
+    buffer.width = width;
+    buffer.height = height;
+
+    // drawImage(source, source_X, source_Y, source_Width, source_Height, dest_X, dest_Y, dest_Width, dest_Height)
+    ctx.drawImage(canvas, offsetX, offsetY, width, height, 0, 0, buffer.width, buffer.height);
+
+    if(callback) callback(buffer.toDataURL('image/png'));
 };
 
 Utils.getData = function (url) {
@@ -702,7 +860,7 @@ Utils.negArray = function (arr) {
     });
 };
 
-Utils.onChange = function (object, callback) {
+Utils.onChange = function(object, changedCallback, callback) {
     /** https://github.com/sindresorhus/on-change */
     'use strict';
     const handler = {
@@ -792,33 +950,15 @@ window['_'] = Utils;
 
 
 // eslint-disable-next-line no-unused-vars
-function connectElgatoStreamDeckSocket (
-    inPort,
-    inUUID,
-    inMessageType,
-    inApplicationInfo,
-    inActionInfo
-) {
+function connectElgatoStreamDeckSocket(inPort, inUUID, inMessageType, inApplicationInfo, inActionInfo) {
     StreamDeck.getInstance().connect(arguments);
     window.$SD.api = Object.assign({ send: SDApi.send }, SDApi.common, SDApi[inMessageType]);
 }
 
 /* legacy support */
 
-function connectSocket (
-    inPort,
-    inUUID,
-    inMessageType,
-    inApplicationInfo,
-    inActionInfo
-) {
-    connectElgatoStreamDeckSocket(
-        inPort,
-        inUUID,
-        inMessageType,
-        inApplicationInfo,
-        inActionInfo
-    );
+function connectSocket(inPort, inUUID, inMessageType, inApplicationInfo, inActionInfo) {
+    connectElgatoStreamDeckSocket(inPort, inUUID, inMessageType, inApplicationInfo, inActionInfo);
 }
 
 /**
@@ -841,20 +981,11 @@ const StreamDeck = (function () {
             inMessageType,
             inApplicationInfo,
             inActionInfo,
-            websocket = null;
+            websocket = null,
+            filterPIMessages = ['didReceiveSettings', 'didReceiveGlobalSettings', 'propertyInspectorDidAppear', 'propertyInspectorDidDisappear'];
+        // filterPIMessages = ['didReceiveGlobalSettings', 'propertyInspectorDidAppear', 'propertyInspectorDidDisappear'];
 
         var events = ELGEvents.eventEmitter();
-        var logger = SDDebug.logger();
-
-        function showVars () {
-            debugLog('---- showVars');
-            debugLog('- port', inPort);
-            debugLog('- uuid', inUUID);
-            debugLog('- messagetype', inMessageType);
-            debugLog('- info', inApplicationInfo);
-            debugLog('- inActionInfo', inActionInfo);
-            debugLog('----< showVars');
-        }
 
         function connect (args) {
             inPort = args[0];
@@ -862,11 +993,6 @@ const StreamDeck = (function () {
             inMessageType = args[2];
             inApplicationInfo = Utils.parseJson(args[3]);
             inActionInfo = args[4] !== 'undefined' ? Utils.parseJson(args[4]) : args[4];
-
-            /** Debug variables */
-            if (debug) {
-                showVars();
-            }
 
             const lang = Utils.getProp(inApplicationInfo,'application.language', false);
             if (lang) {
@@ -933,6 +1059,12 @@ const StreamDeck = (function () {
                     m = jsonObj.event;
                     // console.log('%c%s', 'color: white; background: red; font-size: 12px;', '[common.js]onmessage:', m);
                 } else {
+                    const e = jsonObj['event'];
+                    if(filterPIMessages.includes(e)) {
+                        // console.log('%c%s', 'color: white; background: red; font-size: 13px;', 'EMIITING GLOBAL PIMESSAGE ', e, jsonObj);
+                        events.emit(e, jsonObj);
+                        // m = jsonObj['action'] + '.' + jsonObj['event'];
+                    }
                     switch (inMessageType) {
                     case 'registerPlugin':
                         m = jsonObj['action'] + '.' + jsonObj['event'];
@@ -946,8 +1078,7 @@ const StreamDeck = (function () {
                     }
                 }
 
-                if (m && m !== '')
-                    events.emit(m, jsonObj);
+                if(m && m !== '') events.emit(m, jsonObj);
             };
 
             instance.connection = websocket;
@@ -962,7 +1093,6 @@ const StreamDeck = (function () {
             connection: websocket,
             connect: connect,
             api: null,
-            logger: logger
         };
     }
 
@@ -1074,15 +1204,7 @@ const SDApi = {
          * DEBUG-Utility to quickly show the current payload in the Property Inspector.
          */
 
-        if (
-            $SD.connection &&
-            [
-                'sendToPropertyInspector',
-                'showOK',
-                'showAlert',
-                'setSettings'
-            ].indexOf(fn) === -1
-        ) {
+        if($SD.connection && ['sendToPropertyInspector', 'showOK', 'showAlert', 'setSettings'].indexOf(fn) === -1) {
             // console.log("send.sendToPropertyInspector", payload);
             // this.sendToPropertyInspector(context, typeof payload.payload==='object' ? JSON.stringify(payload.payload) : JSON.stringify({'payload':payload.payload}), pl['action']);
         }
@@ -1177,6 +1299,7 @@ const SDApi = {
         },
 
         setSettings: function (context, payload) {
+            if(!context) context = $SD.uuid;
             SDApi.send(context, 'setSettings', {
                 payload: payload
             });
@@ -1199,13 +1322,14 @@ const SDApi = {
                 const context = inContext ? inContext : $SD.uuid;
                 const device = inDeviceID;
                 const event = 'switchToProfile';
-                if(inProfileName && inProfileName.length !== 0) {
+                // if (inProfileName && inProfileName.length !== 0) {
                     const payload = {
                         profile: inProfileName
                     };
-                    const pl = Object.assign({}, {event, context, device}, payload);
+                const pl = Object.assign({}, {event, context, device}, {payload: payload});
+                console.log("$SD.switchToProfile", inProfileName, pl);
                     $SD.connection && $SD.connection.sendJSON(pl);
-                }
+                // }
             }
         },
 
@@ -1261,30 +1385,6 @@ const SDApi = {
             console.log(this, fn, typeof this[fn], this[fn]());
         }
 
-    }
-};
-
-/** SDDebug
- * Utility to log the JSON structure of an incoming object
- */
-
-const SDDebug = {
-    logger: function (name, fn) {
-        const logEvent = jsn => {
-            console.log('____SDDebug.logger.logEvent');
-            console.log(jsn);
-            debugLog('-->> Received Obj:', jsn);
-            debugLog('jsonObj', jsn);
-            debugLog('event', jsn['event']);
-            debugLog('actionType', jsn['actionType']);
-            debugLog('settings', jsn['settings']);
-            debugLog('coordinates', jsn['coordinates']);
-            debugLog('---');
-        };
-
-        const logSomething = jsn => console.log('____SDDebug.logger.logSomething');
-
-        return { logEvent, logSomething };
     }
 };
 
